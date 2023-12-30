73d1fad225bbf3c7afc465a0628ff5b1efc2d659
Inaam Ul Haq <inaamulhaq7847@gmail.com>
2023-12-31 02:25:25 +0500

    Updates

---
+++ android/app/src/main/kotlin/com/urbank/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.urbank.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:urbank/core/app_export.dart';
+
+class ApiClient extends GetConnect {}
