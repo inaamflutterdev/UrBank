3c795b50ac3962c4fb7e59f61589aa6bd8803a8d
Inaam Ul Haq <inaamulhaq7847@gmail.com>
2024-01-08 22:19:44 +0500

    Updates

--- lib/presentation/country_or_region_screen/models/country_or_region_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-import 'listunitedkingdom_item_model.dart';
-
-class CountryOrRegionModel {
-  Rx<List<ListunitedkingdomItemModel>> listunitedkingdomItemList =
-      Rx(List.generate(7, (index) => ListunitedkingdomItemModel()));
-}
