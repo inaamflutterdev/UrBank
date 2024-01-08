0dd70a3601c1648ba9de14c2b853bd75f035d5c1
Inaam Ul Haq <inaamulhaq7847@gmail.com>
2024-01-08 22:20:35 +0500

    Updates

--- lib/presentation/mine_page_container_screen/binding/mine_page_container_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/mine_page_container_controller.dart';
-import 'package:get/get.dart';
-
-class MinePageContainerBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => MinePageContainerController());
-  }
-}
--- lib/presentation/national_bank_page/models/nationalbank_item_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class NationalbankItemModel {
-  Rx<String> priceTxt = Rx("-220.00");
-
-  Rx<String> timeTxt = Rx("10:30 PM, 18 June, 2022");
-
-  Rx<String>? id = Rx("");
-}
